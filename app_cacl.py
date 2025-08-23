a8138fc5dd59ff3209093ce57954b93eba1c3690
sinclair <fujibook1108@gmail.com>
2025-08-24 01:26:54 +0900

    計算部分を作成

---
+++ app_cacl.py
@@ -0,0 +1,46 @@
+﻿import pandas as pd
+
+#app_cacl.py
+session_state = {"線形計画法":"秀","情報システム・セキュリティ概論":"優"}
+#補遺
+#線形計画法：A
+#情報システム・セキュリティ概論：D,C
+
+#category.csv = 区分が記載されたcsvファイル
+cat = pd.read_csv("category.csv", encoding="cp932") #名前を付けて保存したら文字コードがShift-JISだったので、それを指定
+grades = {"A":0, "B":0, "C":0, "D":0, "E":0, "F":0} #得点部分の集計用
+gpa = {"不可":0, "可":1, "良":1.4, "優":1.8, "秀":2} #評価ごとのポイント
+total = {"A":0, "B":0, "C":0, "D":0, "E":0, "F":0} #どのカテゴリのものを何単位履修したのか集計する
+cacl_result = {"A":0, "B":0, "C":0, "D":0, "E":0, "F":0} #結果
+
+for key,value in session_state.items():
+    for number in range (len(cat)):
+        #print("!")
+        if cat["科目名"].iloc[number] == key:
+            #print(key)
+            #category = cat.loc[cat["科目名"] == key].values[0]
+            category_list = cat.loc[cat["科目名"] == key, "カテゴリ"].str.split(",").values[0]
+
+            #print(category_list[0])
+            subject_grade = gpa[value]
+            #print(subject_grade)
+            grades[category_list[0]] += subject_grade
+            total[category_list[0]] += 1
+            try:
+                if category_list[1] != None:
+                    grades[category_list[1]] += subject_grade
+                    total[category_list[1]] += 1
+            except:
+                pass
+            break
+
+#print(grades)
+#print(total)
+
+for key in cacl_result:
+    #print(grades[key], total[key])
+    try:
+        cacl_result[key] = grades[key] / total[key]
+    except:
+        pass
+print(cacl_result)
